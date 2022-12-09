60ab665a2c5dc4c862381cad8ef7beef0507da4c
Olga-Melnyk <3717129@ukr.net>
2022-12-09 22:06:56 +0200

    adds task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector('.change-color');
+console.dir(button);
+
+const span = document.querySelector('.color');
+console.dir(span);
+
+const body = document.querySelector('body');
+console.dir(body);
+
+button.addEventListener('click', onClick);
+
+function onClick(evt) {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+}
