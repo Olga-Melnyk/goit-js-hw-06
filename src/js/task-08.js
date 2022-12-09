e8bb1493119661b25ac738e1b879cd7f43e8b8b9
Olga-Melnyk <3717129@ukr.net>
2022-12-09 21:33:16 +0200

    adds task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  if (!formElements.email.value || !formElements.password.value) {
+    return alert('All fields must be filled');
+  } else {
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+  }
+}
